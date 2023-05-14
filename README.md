9dc1d4f378fbdbe2398f440a3c8a88b5efe5f16c
crispim <crispim1411@gmail.com>
2023-05-13 23:20:40 -0300

    movidos projetos como binarios de um projeto só

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Maelstrom Challenges
+# Maelstrom ChaUllenges
 Solução dos exercícios de Sistemas Distribuidos disponíveis em [fly.io/dist-sys](https://fly.io/dist-sys/)
 
 ## #1 Echo
@@ -6,14 +6,32 @@ Implementar um sistema que receba e devolva o pacote. O tipo da mensagem deverá
 
 [solução](https://github.com/crispim1411/maelstrom_challenges_rust/blob/master/echo/src/main.rs)
 
+Run:
+```
+./maelstrom/maelstrom test -w echo --bin dist-sys-rust/target/debug/echo --node-count 1 --time-limit 10 
+```
+
 ## #2 Unique Id
 Utilizando do mesma sistema anterior responder a mensagem 'generate' com 'generate_ok' retornando um Id único global.
 
 [solução](https://github.com/crispim1411/maelstrom_challenges_rust/blob/master/unique_id/src/main.rs)
 
+Run:
+```
+./maelstrom/maelstrom test -w unique-ids --bin dist-sys-rust/target/debug/unique_id --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+```
+
+
 ## #3 Broadcast
 Implementar um sitema de broadcast para a fofoca circular entre todos os nodes do cluster.
 
-### 3a - Single Node
-
 [solução](https://github.com/crispim1411/maelstrom_challenges_rust/blob/master/broadcast/src/main.rs)
+
+### 3a - Single Node
+Primeiro cenário apenas considerando um sistema de um Node.
+
+Run:
+```
+./maelstrom/maelstrom test -w broadcast --bin dist-sys-rust/target/debug/broadcast --node-count 1 --time-limit 20 --rate 10
+```
+
---
+++ dist-sys-rust/src/bin/broadcast.rs
@@ -0,0 +1,117 @@
+use serde::{Deserialize, Serialize};
+use std::{io::{self, StdoutLock, Write}, collections::HashMap};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag="type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    // Requests
+    Init {
+        msg_id: usize,
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    Read {
+        msg_id: usize,
+    },
+    Broadcast {
+        msg_id: usize,
+        message: usize 
+    },
+    Topology { 
+        msg_id: usize,
+        topology: HashMap<String, Vec<String>> 
+    },
+    // Responses
+    InitOk {
+
+        in_reply_to: usize,
+    },
+    BroadcastOk {
+
+        in_reply_to: usize,
+    },
+    ReadOk { 
+        in_reply_to: usize,
+
+        messages: Vec<usize> 
+    },
+    TopologyOk {
+
+        in_reply_to: usize,
+
+    },
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dst: String,
+    body: Payload,
+}
+
+
+#[derive(Default)]
+struct Node {
+    id: usize,
+    values: Vec<usize>,
+}
+
+
+impl Node {
+    fn new(id: usize) -> Self {
+        Node { id, values: vec![] }
+    }
+
+    fn process(&mut self, msg: Message, output: &mut StdoutLock) -> io::Result<()> {
+        let response_body = match msg.body {
+            Payload::Init { msg_id, .. } => {
+                Payload::InitOk { in_reply_to: msg_id } 
+            }
+            Payload::Read { msg_id } => {
+                Payload::ReadOk { 
+                    in_reply_to: msg_id,
+                    messages: self.values.clone(), 
+                }
+            }
+            Payload::Broadcast { msg_id, message } => {
+                self.values.push(message);
+                Payload::BroadcastOk { in_reply_to: msg_id }
+            }
+            Payload::Topology { msg_id, .. } => {
+                Payload::TopologyOk { in_reply_to: msg_id }
+            }
+            _m => panic!("Should not receive {:?} message", _m),
+        };
+
+        let response = Message {
+            src: msg.dst,
+            dst: msg.src,
+            body: response_body, 
+        };
+        serde_json::to_writer(&mut *output, &response)?;
+        output.write_all(b"\n")?;
+
+        Ok(())
+    }
+}
+
+fn main() -> io::Result<()> {
+    // initialization
+    let stdin = io::stdin().lock();
+    let mut stdout = io::stdout().lock();
+
+    let mut node = Node::new(0);
+
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    for input in inputs {
+        let input = input.expect("Error deserializing message");
+        node.process(input, &mut stdout)
+            .expect("Error processing message");
+        node.id += 1;
+    }
+    Ok(())
+}
