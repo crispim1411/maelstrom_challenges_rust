f7e08df6b0f9295cc7c62b9cc9465faefed86507
Rodrigo Crispim <crispim1411@gmail.com>
2025-05-10 19:19:42 -0300

    Melhoria grow-only

--- dist-sys-rust/src/bin/grow_counter.rs
+++ dist-sys-rust/src/bin/grow_counter.rs
@@ -2,6 +2,7 @@ use anyhow::bail;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::sync::mpsc;
+use std::time::Duration;
 use std::thread;
 use std::io::{StdoutLock, Write, self, StdinLock, Lines};
 
@@ -63,6 +64,9 @@ enum Payload {
     Error {
         code: usize,
         text: String,
+    },
+    Gossip { 
+        value: usize 
     }
 }
 
@@ -84,24 +88,34 @@ struct Init {
 enum Event {
     Message(Message<Payload>),
     Read,
-    Cas(usize),
     Eof,
 }
 
 struct Node {
     id: usize, 
     node_id: String,
-    event_sender: mpsc::Sender<Event>,
+    neighbors: Vec<String>,
     counter: usize,
     last_value: usize,
 }
 
 impl Node {
     fn from_init(message: Init, tx: mpsc::Sender<Event>) -> Self {
+        thread::spawn(move || {
+            loop { 
+                thread::sleep(Duration::from_millis(300));
+                if tx.send(Event::Read).is_err() {
+                    break;
+                }
+            }
+        });
         Node {
             id: 1,
+            neighbors: message.node_ids
+                .into_iter()
+                .filter(|n| n != &message.node_id)
+                .collect(),
             node_id: message.node_id,
-            event_sender: tx,
             counter: 0,
             last_value: 0,
         }
@@ -115,25 +129,26 @@ impl Node {
                     value: self.counter
                 };
                 reply.send(output)?;
-                self.event_sender.send(Event::Read)?;
             }
             Payload::Add { delta } => {
                 if delta > 0 {
                     self.last_value = delta;
-                    self.event_sender.send(Event::Cas(delta))?;    
+                    self.add_to_store(delta, output)?;
+                    self.counter += delta;
+                    self.send_gossip(self.last_value, output)?;
                 }
                 reply.body.payload = Payload::AddOk;
                 reply.send(output)?;
             }
+            Payload::Gossip { value } => {
+                self.counter += value;
+            }
             Payload::ReadOk { value } => {
                 self.counter = value;
             }
-            Payload::CasOk => {
-                self.event_sender.send(Event::Read)?;
-            }
             Payload::Error { code, text } => {
                 if code == 20 {
-                    self.event_sender.send(Event::Cas(0))?;
+                    self.add_to_store(0, output)?;
                 }
                 else if code == 22 {
                     let re = Regex::new(ERROR_MSG).unwrap();
@@ -141,11 +156,14 @@ impl Node {
                         if let Ok(current_value) = matches["current"].parse::<usize>() {
                             self.counter = current_value;
                         }
-                        self.event_sender.send(Event::Cas(self.last_value))?;
+                        if self.last_value > 0 {
+                            self.add_to_store(self.last_value, output)?;
+                            self.send_gossip(self.last_value, output)?;
+                        }
                     } 
                 }
             }
-            Payload::AddOk | Payload::Cas { .. } => {},
+            Payload::AddOk | Payload::Cas { .. } | Payload::CasOk => {},
         }
         self.id += 1;
         Ok(())
@@ -154,40 +172,61 @@ impl Node {
     fn handle_event(&mut self, event: Event, output: &mut StdoutLock) -> anyhow::Result<()> {
         match event { 
             Event::Eof => {}
-            Event::Cas(delta) => {
-                let message = Message {
-                    src: self.node_id.clone(),
-                    dst: "lin-kv".to_string(),
-                    body: Body { 
-                        msg_id: None, 
-                        in_reply_to: None, 
-                        payload: Payload::Cas { 
-                            key: KEY.to_string(),
-                            from: self.counter, 
-                            to: self.counter + delta,
-                            create_if_not_exists: true
-                        }
-                    }
-                };
-                message.send(output)?;
-            }
             Event::Read => {
-                let message = Message {
-                    src: self.node_id.clone(),
-                    dst: LIN_KV.to_string(),
-                    body: Body { 
-                        msg_id: None, 
-                        in_reply_to: None, 
-                        payload: Payload::Read {
-                            key: Some(KEY.to_string())
-                        }
-                    }
-                };
-                message.send(output)?;
+                self.read_from_store(output)?;
             }
             Event::Message(msg) => {
                 self.process(msg, output)?;
             }
+        }
+        Ok(())
+    }
+
+    fn read_from_store(&self, output: &mut StdoutLock) -> anyhow::Result<()> {
+        let message = Message {
+            src: self.node_id.clone(),
+            dst: LIN_KV.to_string(),
+            body: Body { 
+                msg_id: None, 
+                in_reply_to: None, 
+                payload: Payload::Read {
+                    key: Some(KEY.to_string())
+                }
+            }
+        };
+        return message.send(output);
+    }
+
+    fn add_to_store(&self, delta: usize, output: &mut StdoutLock) -> anyhow::Result<()> {
+        let message = Message {
+            src: self.node_id.clone(),
+            dst: "lin-kv".to_string(),
+            body: Body { 
+                msg_id: None, 
+                in_reply_to: None, 
+                payload: Payload::Cas { 
+                    key: KEY.to_string(),
+                    from: self.counter, 
+                    to: self.counter + delta,
+                    create_if_not_exists: true
+                }
+            }
+        };
+        return message.send(output);
+    }
+
+    fn send_gossip(&self, value: usize, output: &mut StdoutLock) -> anyhow::Result<()> {
+        for neighbor in self.neighbors.iter() {
+            let message = Message {
+                src: self.node_id.clone(),
+                dst: neighbor.clone(),
+                body: Body { 
+                    msg_id: None, 
+                    in_reply_to: None, 
+                    payload: Payload::Gossip { value } 
+                }
+            };
+            message.send(output)?;
         }
         Ok(())
     }
